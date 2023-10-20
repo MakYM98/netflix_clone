671b83359495770efca8bfbe8ed69461077c98d3
YeongMeng <makyeongmeng@gmail.com>
2023-10-20 12:39:46 +0800

    Movie Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import Navbar from './components/navbar'
 import Billboard from './components/billboard'
+import MovieList from './components/movieList'
+import movies from "./config/movies.json" assert { type: 'json' };; 
 
 export default function Home() {
+  const data = movies
+
   return (
     <>
       <Navbar/>
       <Billboard/>
+      <div className='pb-40'>
+        <MovieList title="Trending Now" data={data}/>
+      </div>
     </>
   )
 }
